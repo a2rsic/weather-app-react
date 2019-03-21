800d6c13e6c5ec673bd3158f7a916638f00a4828
Ana <angy.aa@gmail.com>
2019-03-21 13:18:02 +0100

    Add header component and css style

--- src/view/App.js
+++ src/view/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import '../App.css';
+import { SearchLocation } from './SearchLocation';
+import { Header } from './common/Header';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App works
+        <Header />
+        <SearchLocation />
       </div>
     );
   }
---
+++ src/view/SearchLocation.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class SearchLocation extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>Search component</div>
+        )
+    }
+}
+
+export {
+    SearchLocation
+}
